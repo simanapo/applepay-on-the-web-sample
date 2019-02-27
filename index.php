6a8cddfd084894256823d0fd12a112512fe308ba
simanapo <kitamori6@yahoo.co.jp>
2019-02-27 11:47:09 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ApplePay on the Web Testt</title>
+  </head>
+  <body>
+    <div class="button-modal-area">
+      <button type="button" id="button_modal_applepay" style="display:none;">
+        <span class="unit"></span>
+      </button>
+    </div>
+    <script src="/web/assets/js/applepay.js"></script>
+  </body>
+</html>
